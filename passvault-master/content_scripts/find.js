8504544d20f4cd4dd1ed5a55fbc30035655ed668
vinodhan16 <vinodhan.thunderbolt@gmail.com>
2019-06-19 17:26:58 +0530

    ajax

--- passvault-master/content_scripts/find.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("beg").addEventListener('click', function(e) {
-    browser.runtime.sendMessage({msg:"start"});
-  });
-  document.getElementById("prod").addEventListener('click', function(e) {
-      browser.runtime.sendMessage({msg:"prod"});
-    });
-    document.getElementById("reset").addEventListener('click', function(e) {
-        browser.runtime.sendMessage({msg:"reset"});
-      });
--- passvault-master/panel/panel.js
+++ passvault-master/panel/panel.js
@@ -1,48 +1,15 @@
-function startTimer(duration, display) {
-  var diff,minutes,seconds;
-  var start=Date.now();
-    function timer() {
-        diff = duration - (((Date.now() - start) / 1000) | 0);
-        minutes = (diff / 60) | 0;
-        seconds = (diff % 60) | 0;
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = minutes + ":" + seconds;
-        if (diff <= 0) {
-            start = Date.now() + 1000;
-        }
-    };
-    timer();
-    setInterval(timer, 1000);
-
-}
-if(document.readyState==="loading"){
-    browser.runtime.sendMessage({msg:"load"});
-}
-browser.runtime.onMessage.addListener(handleMessage);
-function handleMessage(request) {
-  if(request.msg==="start"){
-    var min=request.min;
-    var sec=request.sec;
-    var duration=(min*60)+sec;
-    var progress=100-Math.ceil(duration/48);
-    document.getElementById("myBar").style.width=progress+"%";
-    document.getElementById("myBar").innerHTML=progress+"%";
-    var display = document.querySelector('#time');
-    startTimer(duration,display);
-  }
-  if(request.msg==="prod"){
-    var min=request.min;
-    var sec=request.sec;
-    var duration=(min*60)+sec;
-    var mins=Math.floor(duration/60);
-    var secs= duration%60;
-    mins = mins < 10 ? "0" + mins : mins;
-    secs = secs < 10 ? "0" + secs : secs;
-    var display = document.querySelector('#time');
-    var progress=100-Math.ceil(duration/48);
-    document.getElementById("myBar").style.width=progress+"%";
-    document.getElementById("myBar").innerHTML=progress+"%";
-    display.textContent = mins + ":" + secs;
-  }
+document.getElementById("submit").onclick = function(){
+  var mail = document.getElementById("mailid").value;
+  var number = document.getElementById("mobile").value;
+  var pin = document.getElementById("pin").value;
+  var props = "mailid="+mail+"&mobile="+number+"&pin="+pin;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.response);
+    }
+  };
+  xhttp.open("POST", "http://ishu.890m.com/myvaultextn/connection.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(props);
 }
