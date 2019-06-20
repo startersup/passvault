e3495daa4e265dc6cd20815368da76109a5fdf70
saicharan96 <saicharan14996@gmail.com>
2019-06-20 15:30:38 +0530

    12233
    ff

--- passvault-master/panel/panel.js
+++ passvault-master/panel/panel.js
@@ -1,3 +1,7 @@
+
+
+
+
 document.getElementById("submit").onclick = function(){
   var mail = document.getElementById("mailid").value;
   var number = document.getElementById("mobile").value;
@@ -6,7 +10,14 @@ document.getElementById("submit").onclick = function(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      console.log(this.response);
+      var data=JSON.parse(this.response);
+        browser.cookies.set({
+    url: "http://www.passvault.com",
+    name: "pv-user-id",
+    value: data.userid
+  });
+        console.log(decodeURIComponent(data.ui));
+        document.getElementById("change").innerHTML=decodeURIComponent(data.ui);
     }
   };
   xhttp.open("POST", "http://ishu.890m.com/myvaultextn/connection.php", true);
